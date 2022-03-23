f626469164fdc0024d9ef0ff25e08778ebc106e4
KyokoKamiya <72701857+KyokoKamiya@users.noreply.github.com>
2022-03-23 04:16:06 +0100

    Update replyHandler.js

--- tools/replyHandler.js
+++ tools/replyHandler.js
@@ -1,20 +1,54 @@
+const { Permissions } = require("discord.js");
+const embeds = require("./embeds");
 
-function replyHandler(messageObject, replyType, replyContent ) {
-    //check if bot has permissios to send messages and embeds in a given channel
-        // message.reoply "error not enough permissions"
-    //if bot has permissios to send messages and embeds
-    
-    //switch statement for different replytypes
-        //default: 
-        // message.reply uknown error
-        //console.log(unkown reply type)
+async function replyHandler(
+   messageObject,
+   replyType,
+   replyTitle,
+   replyContent
+) {
+   const channelPermissions = messageObject.channel.permissionsFor(
+      messageObject.guild.me,
+      true
+   );
 
-    
-    //message.reply({embeds: []})
+   //check if bot has permissios to send messages and embeds in a given channel
+   if (!channelPermissions.has(Permissions.FLAGS.SEND_MESSAGES)) {
+      try {
+         const dmChannel = await messageObject.author.createDM();
+         dmChannel.send({
+            embeds: [
+               embeds.errorEmbed(
+                  "error",
+                  `I do not have permissions to send messages in that channel, \nif you believe this is an error contact a server administrator`
+               ),
+            ],
+         });
+         return;
+      } catch (err) {
+         console.log(err);
+      }
+   }
+   //if bot has permissios to send messages and embeds
 
+   //switch statement for different replytypes
+   switch (replyType) {
+      case "answer":
+         messageObject.reply({
+            embeds: [embeds.answerEmbed(replyTitle, replyContent)],
+         });
+      case "error":
+         messageObject.reply({
+            embeds: [embeds.errorEmbed(replyTitle, replyContent)],
+         });
+
+      default:
+   }
+
+   // message.reply uknown error
+   //console.log(unkown reply type)
+
+   //message.reply({embeds: []})
 }
 
-
-
-
-exports.replyHandler = replyHandler
+exports.reply = replyHandler;
